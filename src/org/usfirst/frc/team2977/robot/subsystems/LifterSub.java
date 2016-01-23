53b40acab8599cd841c1d2f334ef2e27c14ab30e
KingAlex42 <mrquadratic2@gmail.com>
2016-01-23 14:35:32 -0600

    Fixed Naughty Objects

--- src/org/usfirst/frc/team2977/robot/subsystems/LifterSub.java
+++ src/org/usfirst/frc/team2977/robot/subsystems/LifterSub.java
@@ -11,19 +11,19 @@ import edu.wpi.first.wpilibj.command.Subsystem;
  */
 public class LifterSub extends Subsystem {
     
-	public Jaguar hooker = new Jaguar(5);
-	double hookspeed = hooker.get();
+	public Jaguar lifter = new Jaguar(5);
+	double hookspeed = lifter.get();
 	
 	public void Lift() {
-		hooker.set(1);
+		lifter.set(1);
 	}
 	
 	public void deLift() {
-		hooker.set(-1);  
+		lifter.set(-1);  
 	}
 	
 	public void stopLifting() {
-		hooker.set(0); 
+		lifter.set(0); 
 	}
 	
 
