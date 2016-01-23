118d925dc9e8c1ad341b7c8347d598137a180c62
KingAlex42 <mrquadratic2@gmail.com>
2016-01-23 13:24:33 -0600

    thanks Obama

--- src/org/usfirst/frc/team2977/robot/OI.java
+++ src/org/usfirst/frc/team2977/robot/OI.java
@@ -10,7 +10,7 @@ import org.usfirst.frc.team2977.robot.commands.ExampleCommand;
  */
 public class OI {
 	
-	Joystick stick = new Joystick(0);
+//thanks Obama
     //// CREATING BUTTONS
     // One type of button is a joystick button which is any button on a joystick.
     // You create one by telling it which joystick it's on and which button
