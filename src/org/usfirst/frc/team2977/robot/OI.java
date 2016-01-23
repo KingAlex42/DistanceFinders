63b6c7c849ba86d3ae78063dfadb3e328ce11253
JamesRK <FunkyFunky044@gmail.com>
2016-01-23 13:37:58 -0600

    Revert "Jarret's Commit"
    This reverts commit 4684432be3a86cf829ec4340c50d189ac5304930.

--- src/org/usfirst/frc/team2977/robot/OI.java
+++ src/org/usfirst/frc/team2977/robot/OI.java
@@ -1,6 +1,5 @@
 package org.usfirst.frc.team2977.robot;
 
-import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj.buttons.Button;
 import org.usfirst.frc.team2977.robot.commands.ExampleCommand;
 
@@ -9,8 +8,6 @@ import org.usfirst.frc.team2977.robot.commands.ExampleCommand;
  * interface to the commands and command groups that allow control of the robot.
  */
 public class OI {
-	
-	Joystick stick = new Joystick(0);
     //// CREATING BUTTONS
     // One type of button is a joystick button which is any button on a joystick.
     // You create one by telling it which joystick it's on and which button
