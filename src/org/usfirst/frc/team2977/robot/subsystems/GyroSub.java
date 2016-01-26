df6bbdedb0fbfb2f827f7d7898ed1ce0d29c14ee
JamesRK <FunkyFunky044@gmail.com>
2016-01-26 15:47:23 -0600

    NEW HOT GYRO CODE

--- src/org/usfirst/frc/team2977/robot/subsystems/GyroSub.java
+++ src/org/usfirst/frc/team2977/robot/subsystems/GyroSub.java
@@ -17,8 +17,7 @@ public class GyroSub extends Subsystem {
 	double angle; // radians? no degrees lol	
 	double constant = .25; // big motor speed (gotta go fest)
 	double factor = .5; // GCF
-	double i = Math.sqrt(-1); // the skware route of negative juann is aye 
-
+	
 	public void Reset() { 
 			gyro.reset();
 			SmartDashboard.putBoolean("Resetted", true);
@@ -27,15 +26,36 @@ public class GyroSub extends Subsystem {
     public void GyroDrive() { 
     	angle = gyro.getAngle();
     	adjust = constant * (factor * angle);
-    	
+    
     	SmartDashboard.putNumber("Angle", angle);
-
-       		Robot.chassis.m3.set(-(constant + adjust));  // right side
-       		Robot.chassis.m1.set(-(constant + adjust));
-   			Robot.chassis.m4.set(constant - adjust);	 // left side
-   			Robot.chassis.m2.set(constant - adjust);
+    	
+    	if (angle < 0);
+    		AdjustLeftSide();
+    		
+    	if (angle > 0);
+    		AdjustRightSide();
+    		
+    	if (angle == 0);
+    		SetEqual();
+    }
+    	
+    	public void AdjustRightSide() {
+    		Robot.chassis.m3.set(constant + adjust);  // right side
+       		Robot.chassis.m1.set(constant + adjust);
+    	}
+    
+       	public void AdjustLeftSide() {
+   			Robot.chassis.m4.set(constant + adjust);	 // left side
+   			Robot.chassis.m2.set(constant + adjust);
     		//Robot.memeBase.meme1.set(dank);
     	}
+       	public void SetEqual() {
+       		Robot.chassis.m1.set(constant);
+       		Robot.chassis.m2.set(constant);
+       		Robot.chassis.m3.set(constant);
+       		Robot.chassis.m4.set(constant);
+
+       	}
 
     // Put the memes for controlling this planet
     // here. Call these from 4Chan.
